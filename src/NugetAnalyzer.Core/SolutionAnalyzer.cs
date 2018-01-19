660133cfb2a683541ecc4ea742be715d823845ba
Alberto Monteiro <alberto.monteiro@live.com>
2018-01-18 22:28:55 -0300

    Initial project structure

---
+++ src/NugetAnalyzer.Core/SolutionAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Environment;
+using static System.IO.Directory;
+using static System.IO.Path;
+
+namespace NugetAnalyzer.Core
+{
+    public class SolutionAnalyzer
+    {
+        public OperationResult Analyze()
+        {
+            var solutionFiles = EnumerateFiles(CurrentDirectory, "*.sln");
+
+            if (solutionFiles.Skip(1).Any())
+                return new OperationResult("More than one solution file was found.");
+
+            var solutionFile = solutionFiles.FirstOrDefault();
+
+            var projectFolders = GetProjectFolders(solutionFile);
+            var packagesConfigs = projectFolders.SelectMany(pf => EnumerateFiles(pf, "packages.config")).ToArray();
+        }
+
+        private IEnumerable<string> GetProjectFolders(string solutionFile)
+        {
+            var projectMatches = Regex.Matches(solutionFile, @"Project.+=\s"".+?"",\s""(?<projectPath>.+?)""");
+            foreach (Match projectMatch in projectMatches)
+                yield return GetDirectoryName(Combine(CurrentDirectory, projectMatch.Groups["projectPath"].Value));
+        }
+    }
+
+    public struct OperationResult
+    {
+        public bool Success { get; set; }
+        public string Error { get; set; }
+
+        public OperationResult(string error) : this()
+        {
+            Error = error;
+        }
+    }
+}
